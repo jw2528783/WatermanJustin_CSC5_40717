452cce92411c10212d6a10f8cc8b1f9e94fb1c86
jw2528783 <jwaterman96@gmail.com>
2015-02-04 11:46:55 -0800

    Signed-off-by: jw2528783 <jwaterman96@gmail.com>

---
+++ Class_Lab/Game of Life/main.cpp
@@ -0,0 +1,115 @@
+/* 
+ * File:   main.cpp
+ * Author: Justin Waterman
+ * Created on February 4, 2015, 10:57 AM
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+//Global Constants
+const int COL=30;
+//Function Prototypes
+void filAray(char [][COL],int,char);
+void prntAry(char [][COL],int);
+void inject(char [][COL],int,int,char);
+void pause(int);
+void gnratn(char [][COL],int,char,char);
+int cntNbor(char [][COL],int,char,int,int);
+
+int main(int argc, char** argv) {
+    const int ROW=10;
+    char array[ROW][COL];
+    int nGen=1;
+    //Fill Array
+    filAray(array,ROW,'*');
+    //Print Array without Glider
+    prntAry(array,ROW);
+    //Inject Glider
+    inject(array,4,13,'X');
+    //Print Glider
+    prntAry(array,ROW);
+    pause(1);
+    //Game of Life for generations stipulated
+    for(int gen=1;gen<=nGen;gen++){
+        gnratn(array,ROW,'*','X');
+        pause(1);
+    }
+    return 0;
+}
+
+void filAray(char a[][COL],int ROW,char d){
+    for(int r=0;r<ROW;r++){
+        for(int c=0;c<COL;c++)
+            a[r][c]=d;
+    }
+}
+
+void prntAry(char a[][COL],int ROW){
+    cout<<endl;
+    for(int r=0;r<ROW;r++){
+        for(int c=0;c<COL;c++){
+            cout<<a[r][c];
+        }
+        cout<<endl;
+    }
+}
+
+void inject(char a[][COL],int pRow,int pCol,char c){
+    a[pRow+0][pCol+1]=c;
+    a[pRow+1][pCol+2]=c;
+    a[pRow+2][pCol+0]=c;
+    a[pRow+2][pCol+1]=c;
+    a[pRow+2][pCol+2]=c;
+    
+}
+
+void pause(int secs){
+    //Start timer
+    int strt=static_cast<unsigned int>(time(0));
+    int end;
+    do{
+        int end=static_cast<unsigned int>(time(0));
+    }while(secs>(end-strt));
+}
+
+void gnratn(char a[][COL],int ROW,char d,char nd){
+    for(int r=1;r<ROW-1;r++){
+        for(int c=1;c<COL-1;c++){
+            int nBr=cntNbor(a,ROW,d,r,c);
+            if(a[r][c]!=d&&nBr<=1)
+                a[r][c]=d;
+            if(a[r][c]!=d&&nBr>=4)
+                a[r][c]=d;
+            if(a[r][c]!=d&&nBr==3)
+                a[r][c]=nd;
+        }
+    }
+}
+
+int cntNbor(char a[][COL],int ROW,char d,int r,int c){
+    //Count your neighbors
+    int count=0;
+    //Top
+    if(a[r-1][c-1]!=d)
+        count++;
+    if(a[r-1][c]!=d)
+        count++;
+    if(a[r-1][c+1]!=d)
+        count++;
+    //Middle
+    if(a[r][c-1]!=d)
+        count++;
+    if(a[r][c+1]!=d)
+        count++;
+    //Bottom
+    if(a[r+1][c-1]!=d)
+        count++;
+    if(a[r+1][c+1]!=d)
+        count++;
+    if(a[r+1][c]!=d)
+        count++;
+    return count;
+}
---
+++ Class_Lab/SortSearch/main.cpp
@@ -0,0 +1,118 @@
+/* 
+ * File:   main.cpp
+ * Author: Justin Waterman
+ * Created on February 4, 2015, 8:09 AM
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//User Libraries
+
+//Global constants
+
+//Function prototypes
+void filAray(int [],int);
+void prntAry(const int [],int,int);
+int  lSearch(int,const int [],int,int);//Linear Search
+int  fndVals(int [],int,int,int[]);//Fill Array with all value positions found
+void prntFnd(const int [],int,int);//Print the found array
+int  cntFnd(const int [],int,int);//Count the number of values found in the array
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
+    //Declare variables and arrays
+    const int ROW=1000;
+    int pFilRow=100,perLine=10;
+    int array[ROW]={};
+    int found[ROW+1]={};
+    //Fill the Arrays
+    filAray(array,pFilRow);
+    //Print the Arrays
+    prntAry(array,pFilRow,perLine);
+    //Find all the values
+    int val=50;
+    cout<<"Where is "<<val<<" found in the array!"<<endl;
+    int nTimes=fndVals(array,pFilRow,val,found);
+    //Print all the values found
+    prntFnd(found,pFilRow,perLine);
+    //How many times was the value found
+    cout<<val<<" was found "<<nTimes<<"="
+        <<cntFnd(array,pFilRow,val)<<" times"<<endl;
+    //Exit stage right
+    exit(0);
+}
+
+int fndVals(int a[],int n,int val,int f[]){
+    //Loop until you find all the elements
+    //in the array
+    int indx,pos=0,count=0;
+    do{
+        indx=lSearch(pos,a,n,val);
+        f[count++]=indx;
+        pos=indx+1;
+    }while(indx>=0&&indx<n);
+    return --count;
+}
+
+int  cntFnd(const int a[],int n,int val){
+    //Loop until you find all the elements
+    //in the array
+    int indx,pos=0,count=0;
+    do{
+        indx=lSearch(pos,a,n,val);
+        pos=indx+1;
+        count++;
+    }while(indx>=0&&indx<n);
+    return --count;
+}
+
+int  lSearch(int posStrt,const int a[],int n,int val){
+    //Loop until you find
+    for(int indx=posStrt;indx<n;indx++){
+        if(val==a[indx])return indx;
+    }
+    return -1;
+}
+
+//Print perLine Columns for the array output by row
+void prntFnd(const int a[],int n,int perLine){
+    cout<<endl;
+    if(a[0]==-1){
+        cout<<"The value was not found!"<<endl;
+        cout<<endl;
+        return;
+    }
+    cout<<"The value was found at these positions!"<<endl;
+    for(int i=0;i<n;i++){
+        if(a[i]==-1){
+            cout<<endl;
+            return;
+        }
+        cout<<a[i]<<" ";
+        if(i%perLine==(perLine-1))cout<<endl;
+    }
+    cout<<endl;
+}
+
+//Print perLine Columns for the array output by row
+void prntAry(const int a[],int n,int perLine){
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+        if(i%perLine==(perLine-1))cout<<endl;
+    }
+    cout<<endl;
+}
+
+//Randomly fill the array with 2-digit numbers
+void filAray(int a[],int n){
+    for(int i=0;i<n;i++){
+        a[i]=rand()%90+10;
+    }
+}
